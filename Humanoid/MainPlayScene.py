b644e3d76262b379b439e43fb96fbf42470f1a25
LAB2LINCH <labyues@gmail.com>
2017-10-29 21:15:31 +0900

    2017/10/29

--- Humanoid/MainPlayScene.py
+++ Humanoid/MainPlayScene.py
@@ -1,4 +1,5 @@
 from pico2d import *
+from random import *
 import game_framework
 import body
 
@@ -6,25 +7,30 @@ name = "MainPlayScene"
 Character = None
 Map = None
 Monster = None
-regenpoint = None
+Regenpoint = None
+Monster = None
+Stage = 1
+gametime = 0
+regentime = 10
+running = False
 
 def enter():
-    global Character, Map
+    global Character, Map, Stage, Regenpoint, Monster, running
     Character = body.character()
     Map = body.map()
-
+    Stage = 1
+    Regenpoint = [(800,171), (1300,171), (600,369), (900,719), (1200,459)]
+    Monster = [body.monster_sub('./res/mon_sub1.png', 20, Regenpoint[randint(0,4)])]
+    running = True
 
 def exit():
     pass
 
-
 def pause():
-    pass
-
+    running = False
 
 def resume():
-    pass
-
+    running = True
 
 def handle_events():
     events = get_events()
@@ -34,27 +40,49 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
                 game_framework.quit()
+            elif event.key == SDLK_z:
+                Character.skill_z()
+            elif event.key == SDLK_x:
+                Character.skill_x()
+            elif event.key == SDLK_c:
+                Character.skill_c()
+            elif event.key == SDLK_v:
+                Character.skill_v()
             elif event.key == SDLK_LEFT:
                 Character.seeside = -1
+                Character.frame = 0
                 Character.state = 'R'
             elif event.key == SDLK_RIGHT:
                 Character.seeside = 1
+                Character.frame = 0
                 Character.state = 'R'
+            elif event.key == SDLK_SPACE:
+                Character.isground = False
         elif event.type == SDL_KEYUP:
-            if event.key == SDLK_LEFT or event.key == SDLK_RIGHT:
+            if ((event.key == SDLK_LEFT) and (Character.seeside == -1)):
+                Character.frame = 0
+                Character.state = 'I'
+            if ((event.key == SDLK_RIGHT) and (Character.seeside == 1)):
+                Character.frame = 0
                 Character.state = 'I'
 
 
 
 def update():
-    global Character
+    global Character, Monster, running, gametime, regentime
     Character.update()
-    pass
+    if running == True:
+        gametime += 1/60
+        if gametime >= regentime:
+            Monster.append(body.monster_sub('./res/mon_sub1.png', 20, Regenpoint[randint(0,4)]))
+            gametime = 0
 
 
 def draw():
-    global Character, Map
+    global Character, Map, Regenpoint, Monster
     clear_canvas()
     Map.draw()
     Character.draw()
+    for mon in Monster:
+        mon.draw()
     update_canvas()
--- Humanoid/body.py
+++ Humanoid/body.py
@@ -13,6 +13,7 @@ class body:
 class character(body):
     def __init__(self):
         body.__init__(self)
+        self.image_char = load_image('./res/char.png')
         self.image_idle = load_image('./res/char_idle.png')
         self.image_run = load_image('./res/char_run.png')
         #update later
@@ -22,34 +23,105 @@ class character(body):
         #self.image_skil3 = load_image('./res/char_idle.png')
         #self.image_skil4 = load_image('./res/char_idle.png')
         self.x, self.y = 210, 163
-        self.jumpheight = 40
-        self.gravity = 2
-        self.spdy = 20
+        self.jumpheight = 90
+        self.spdy = self.jumpheight/15
+        self.gravity = self.spdy / 15
         self.frame = 0
+        self.skill = None
+        self.c_skill_z = 0
+        self.c_skill_c = 0
+        self.c_skill_v = 0
 
     def update(self):
+        if self.c_skill_z > 0:
+            self.c_skill_z -= 1/60
+        if self.c_skill_c > 0:
+            self.c_skill_c -= 1/60
+        if self.c_skill_v > 0:
+            self.c_skill_v -= 1/60
         if self.isground == False:
             self.y += self.spdy
             self.spdy -= self.gravity
+            if self.y < 163:
+                self.y = 163
+                self.spdy = self.jumpheight/15
+                self.isground = True
         if self.state == 'R':
             self.frame = (self.frame + 1) % 8
             self.x += (self.spd * self.seeside)
         elif self.state == 'A':
+            self.frame = (self.frame + 1) % 4
             pass
 
     def draw(self) :
-        if self.isground == False:
+        if self.state == 'A':
             self.image_idle.draw(self.x, self.y)
         elif self.state == 'I':
-            self.image_idle.draw(self.x, self.y)
+            if(self.seeside == -1):
+                self.image_char.clip_draw(0,72,33,36,self.x,self.y)
+            elif(self.seeside == 1):
+                self.image_char.clip_draw(33,72,33,36,self.x,self.y)
+            #self.image_idle.draw(self.x, self.y)
         elif self.state == 'R':
-            self.image_run.clip_draw
-            self.image_run.clip_draw((self.frame*33),0,33,36,self.x,self.y)
-        elif self.state == 'A':
-            self.image_idle.draw(self.x, self.y)
+            if(self.seeside == 1):
+                self.image_char.clip_draw((self.frame*33), 0, 33, 36, self.x, self.y)
+            elif(self.seeside == -1):
+                self.image_char.clip_draw((self.frame*33), 36, 33, 36, self.x, self.y)
+            #self.image_run.clip_draw((self.frame*33),0,33,36,self.x,self.y)
 
-class monster(body):
-    pass
+    def skill_z(self): #move
+        if self.c_skill_z <= 0:
+            self.skill = 'Z'
+            self.x += self.spd * 30 * self.seeside
+            self.c_skill_z = 1.5
+
+    def skill_x(self): #atk
+        self.skill = 'X'
+        self.x += self.spd * 30 * self.seeside
+
+    def skill_c(self):  #6hit
+        if self.c_skill_c <= 0:
+            self.skill = 'C'
+            self.x += self.spd * 30 * self.seeside
+            self.c_skill_c = 3
+
+    def skill_v(self):  #area 3hit
+        if self.c_skill_v <= 0:
+            self.skill = 'V'
+            self.x += self.spd * 30 * self.seeside
+            self.c_skill_v = 5
+
+class monster_body(body):
+    def __init__(self, root, hp, pointXY):
+        body.__init__(self)
+        self.image = load_image(root)
+        self.hp = hp
+        self.x , self.y = pointXY
+
+    def draw(self):
+        if self.seeside == -1:
+            self.image.clip_draw(0, 0, 48, 52, self.x, self.y)
+        elif self.seeside == 1:
+            self.image.clip_draw(48, 0, 48, 52, self.x, self.y)
+
+class monster_sub(monster_body):
+    def __init__(self, root, hp, pointXY):
+        monster_body.__init__(self, root, hp, pointXY)
+
+class monster_main(monster_body):
+    def __init__(self, root, hp, pointXY):
+        monster_body.__init__(self, root, hp, pointXY)
+        self.skill_state = 0
+
+    def draw(self):
+        if self.state == 'I':
+            monster_body.draw(self)
+
+    def update(self):
+        pass
+
+    def patern1(self):
+        self.skill_state = 1
 
 class map():
     def __init__(self):
