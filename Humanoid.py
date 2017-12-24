69c38154322f2d9dce0d3d2bdafce59271d33e52
LAB2LINCH <labyues@gmail.com>
2017-12-24 16:56:27 +0900

    add setup

--- Humanoid.py
+++ Humanoid.py
@@ -1,7 +1,24 @@
-import game_framework
+import platform
+import os
+
+if platform.architecture()[0] == '32bit':
+    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x86"
+else:
+    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x64"
+
 import StartScene
 from pico2d import *
+import game_framework
 
-open_canvas(1600,900,False,False)
-game_framework.run(StartScene)
-close_canvas()
+start_data_file = open('start_data.txt', 'r')
+start_data = start_data_file.readlines()
+start_data_file.close()
+
+if start_data[2][13:] == 'True':
+    open_canvas(1600,900,False,True)
+    game_framework.run(StartScene)
+    close_canvas()
+elif start_data[2][13:] == 'False':
+    open_canvas(1600,900,False,False)
+    game_framework.run(StartScene)
+    close_canvas()
